7e743bb37ac150fc537a13a04ecbce229fdc6416
Kremena Nikolova <aradia1992@gmail.com>
2023-07-04 22:30:50 +0300

    Create Data Models Entities

---
+++ SaraDog-PetShop/PetShop.Data.Models/Item.cs
@@ -0,0 +1,40 @@
+﻿namespace PetShop.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using static PetShop.Common.ValidationConstants.ItemValidationConstants;
+
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MaxLength(ImageMaxLength)]
+        public string Image { get; set; } = null!;
+
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public double Rating { get; set; }
+
+        [ForeignKey(nameof(Category))]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public virtual Category Category { get; set; } = null!;
+
+        public virtual ICollection<Category> Categories { get; set; } = new HashSet<Category>();
+
+        public virtual ICollection<Review> Reviews { get; set; } = new HashSet<Review>();
+
+
+    }
+}
