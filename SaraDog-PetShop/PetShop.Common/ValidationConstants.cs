a5d996ec45a4f5bb80b5350728e03705f9285e49
Kremena Nikolova <aradia1992@gmail.com>
2023-07-04 22:31:03 +0300

    Initial commit

---
+++ SaraDog-PetShop/PetShop.Common/ValidationConstants.cs
@@ -0,0 +1,35 @@
+﻿namespace PetShop.Common
+{
+    public static class ValidationConstants
+    {
+        public static class ItemValidationConstants
+        {
+            public const int TitleMinLength = 5;
+            public const int TitleMaxLength = 20;
+
+            public const int DescriptionMinLength = 10;
+            public const int DescriptionMaxLength = 150;
+
+            public const double PriceMinValue = 0;
+            public const double PriceMaxValue = 500;
+
+            public const double RatingMinValue = 0;
+            public const double RatingMaxValue = 5;
+
+            public const int ImageMaxLength = 2048;
+
+        }
+
+        public static class CategoryValidationConstants
+        {
+            public const int NameMinLength = 5;
+            public const int NameMaxLength = 20;
+        }
+
+        public static class CommentValidationConstants
+        {
+            public const int DescriptionMinLength = 10;
+            public const int DescriptionMaxLength = 500;
+        }
+    }
+}
