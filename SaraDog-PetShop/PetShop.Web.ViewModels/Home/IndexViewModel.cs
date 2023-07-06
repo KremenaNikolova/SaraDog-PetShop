c9959b25c52add78764da3ee9c2d45084ff8d0df
Kremena Nikolova <aradia1992@gmail.com>
2023-07-06 14:13:51 +0300

    Create IndexViewModel which I will use in Shop Service

---
+++ SaraDog-PetShop/PetShop.Web.ViewModels/Home/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace PetShop.Web.ViewModels.Home
+{
+    public class IndexViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string Image { get; set; } = null!;
+
+        public decimal Price { get; set; }
+    }
+}
