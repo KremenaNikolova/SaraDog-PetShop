a951afa542ace6ca2da079add14f6d6ba230c7f9
Kremena Nikolova <aradia1992@gmail.com>
2023-07-06 14:09:53 +0300

    Adding Shop Service

---
+++ SaraDog-PetShop/PetShop.Services.Data/Interfaces/IShopService.cs
@@ -0,0 +1,9 @@
+﻿namespace PetShop.Services.Data.Interfaces
+{
+    using PetShop.Web.ViewModels.Home;
+
+    public interface IShopService
+    {
+        Task<IEnumerable<IndexViewModel>> GetAllItemsAsync();
+    }
+}
---
+++ SaraDog-PetShop/PetShop.Services.Data/ShopService.cs
@@ -0,0 +1,35 @@
+﻿namespace PetShop.Services.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    
+    using PetShop.Services.Data.Interfaces;
+    using PetShop.Web.Data;
+    using PetShop.Web.ViewModels.Home;
+
+    public class ShopService : IShopService
+    {
+        private readonly PetShopDbContext dbContext;
+
+        public ShopService(PetShopDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<IndexViewModel>> GetAllItemsAsync()
+        {
+            var allItems = await dbContext
+                .Items
+                .OrderByDescending(i=>i.AddedOn)
+                .Select(i => new IndexViewModel
+                {
+                    Id = i.Id,
+                    Title = i.Title,
+                    Image = i.TitleImage,
+                    Price = i.Price,
+                })
+                .ToArrayAsync();
+
+            return allItems;
+        }
+    }
+}
