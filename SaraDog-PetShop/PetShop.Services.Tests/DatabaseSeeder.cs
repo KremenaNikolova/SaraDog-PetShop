f294da36ca8cf7ebe1a382355c457ca80c4ba640
Kremena Nikolova <aradia1992@gmail.com>
2023-08-04 14:06:35 +0300

    Add more seed for tests. Fix some warnings

--- SaraDog-PetShop/PetShop.Services.Tests/DatabaseSeeder.cs
+++ SaraDog-PetShop/PetShop.Services.Tests/DatabaseSeeder.cs
@@ -1,13 +1,20 @@
 ﻿namespace PetShop.Services.Tests
 {
+    using Azure.Storage.Blobs.Models;
     using PetShop.Data.Models;
     using PetShop.Web.Data;
 
     public static class DatabaseSeeder
     {
-        public static ApplicationUser User;
-        public static ApplicationUser Moderator;
-        public static ApplicationUser Admin;
+        public static ApplicationUser? User;
+        public static ApplicationUser? Moderator;
+        public static ApplicationUser? Admin;
+
+        public static Item? Item;
+        public static Order? Order;
+        public static Cart? Cart;
+        public static CartItem? CartItem;
+
 
         public static void SeedDatabase(PetShopDbContext dbContext)
         {
@@ -57,6 +64,42 @@
             dbContext.Users.Add(User);
             dbContext.Users.Add(Moderator);
             dbContext.Users.Add(Admin);
+
+            Cart = new Cart()
+            {
+                Id = Guid.Parse("8D7572A5-4C6E-488F-AB8B-0AC01BF6D54A"),
+                UserId = Guid.Parse("7677f019-9433-48df-8e8b-1962d4952402"),
+                CreatedOn = DateTime.Parse("2023-07-28 10:04:42.2284039")
+            };
+
+            CartItem = new CartItem()
+            {
+                CartId = Guid.Parse("8D7572A5-4C6E-488F-AB8B-0AC01BF6D54A"),
+                ItemId = 11,
+                Quantity = 3
+            };
+
+            Cart.CartItems.Add(CartItem);
+
+            dbContext.CartItems.Add(CartItem);
+            dbContext.Carts.Add(Cart);
+
+            Order = new Order()
+            {
+                Id = Guid.Parse("DEFA6A7F-A3EA-4DDF-945D-115F9CB7B246"),
+                FirstName = "Kremena",
+                LastName = "Nikolova",
+                Country = "Bulgaria",
+                City = "Test City",
+                Address = "Test Address",
+                PostCode = "Test 9932",
+                CreatedOn = DateTime.Parse("2023-07-28 08:57:56.9627022"),
+                UserId = Guid.Parse("7677f019-9433-48df-8e8b-1962d4952402"),
+                CartId = Guid.Parse("8D7572A5-4C6E-488F-AB8B-0AC01BF6D54A"),
+                TotalPrice = 29.55M
+            };
+
+            dbContext.Orders.Add(Order);
 
             dbContext.SaveChanges();
         }
