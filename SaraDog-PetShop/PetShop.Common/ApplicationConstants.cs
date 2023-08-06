b76b9a56bcdacf92f94999d5c12de17528897262
Kremena Nikolova <aradia1992@gmail.com>
2023-08-06 12:16:11 +0300

    Add administrator constants

--- SaraDog-PetShop/PetShop.Common/ApplicationConstants.cs
+++ SaraDog-PetShop/PetShop.Common/ApplicationConstants.cs
@@ -7,5 +7,10 @@
         public const int DefaultPage = 1;
         public const int ItemsPerPageConstant = 12;
 
+        public const string AdministratorUsername = "Admin";
+        public const string AdministratorEmail = "administrator@abv.bg";
+        public const string AdministratorPassword = "Admin1234";
+        public const string AdministratorRole = "Admin";
+
     }
 }
